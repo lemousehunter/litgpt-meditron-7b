70fa879d914d8de22abbbc4488f343e3dee26a21
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-06-27 04:16:13 +0800

    Update lora.py

--- litgpt/finetune/lora.py
+++ litgpt/finetune/lora.py
@@ -13,6 +13,7 @@ import warnings
 
 import lightning as L
 import torch
+from datasets import tqdm
 from lightning.fabric.plugins import BitsandbytesPrecision
 from lightning.fabric.strategies import FSDPStrategy
 from lightning.fabric.utilities import ThroughputMonitor
@@ -428,7 +429,10 @@ def get_dataloaders(
 
 def get_longest_seq_length(data: List[Dict]) -> Tuple[int, int]:
     # find out the minimum max_seq_length required during fine-tuning (saves memory!)
-    lengths = [len(d["input_ids"]) for d in data]
+    lengths = list()
+    for d in tqdm(data):
+        lengths.append(len(d["input_ids"]))
+    # lengths = [len(d["input_ids"]) for d in data]
     longest_seq_length = max(lengths)
     longest_seq_ix = lengths.index(longest_seq_length)
     return longest_seq_length, longest_seq_ix
