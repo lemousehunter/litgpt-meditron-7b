1718af6fe72a40fc44cf56e7cdf34d3c35c856c5
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-06-28 02:17:47 +0800

    disable cpu offload
    CPU offload slows down training

--- litgpt/finetune/lora.py
+++ litgpt/finetune/lora.py
@@ -153,7 +153,7 @@ def setup(
             activation_checkpointing_policy={Block},
             state_dict_type="full",
             limit_all_gathers=True,
-            cpu_offload=True,
+            cpu_offload=False,
         )
     else:
         strategy = "auto"
