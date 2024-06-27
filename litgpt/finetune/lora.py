29c45e194ff49ca9c8338d465b5bcc5ef08384c4
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2024-06-27 08:47:20 +0800

    Update lora.py

--- litgpt/finetune/lora.py
+++ litgpt/finetune/lora.py
@@ -269,14 +269,14 @@ def fit(
     tokenizer = Tokenizer(checkpoint_dir)
 
     fabric.print("Getting longest seq length...")
-    longest_seq_length, longest_seq_ix = get_longest_seq_length(ConcatDataset([train_dataloader.dataset, val_dataloader.dataset]))
-    print("longest_seq_length:", longest_seq_length)
-    print("longest_seq_ix:", longest_seq_ix)
-    model.max_seq_length = min(longest_seq_length, train.max_seq_length or float("inf"))
-    fabric.print(
-        f"The longest sequence length in the train data is {longest_seq_length}, the model's maximum sequence length is"
-        f" {model.max_seq_length} and context length is {model.config.block_size}"
-    )
+    # longest_seq_length, longest_seq_ix = get_longest_seq_length(ConcatDataset([train_dataloader.dataset, val_dataloader.dataset]))
+    # print("longest_seq_length:", longest_seq_length)
+    # print("longest_seq_ix:", longest_seq_ix)
+    # model.max_seq_length = min(longest_seq_length, train.max_seq_length or float("inf"))
+    # fabric.print(
+        # f"The longest sequence length in the train data is {longest_seq_length}, the model's maximum sequence length is"
+        # f" {model.max_seq_length} and context length is {model.config.block_size}"
+    # )
 
     if eval.initial_validation:
         val_loss = validate(fabric, model, val_dataloader, dataclasses.replace(eval, max_iters=len(val_dataloader)))
